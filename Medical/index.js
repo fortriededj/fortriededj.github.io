82e3daddf86ecf6a45c483ead441810d18414748
DanFortriede <sysdan@bsu.edu>
2021-07-20 12:06:18 -0400

    Combining Styles and Scripts

--- Medical/index.js
+++ Medical/index.js
@@ -1,6 +1,23 @@
-var timers = new Array();
-var timeOut = 300;		//Delay before executing fixBoxes
-var timeDiff = 5*1000;		//Max age of person page 30 seconds
+// Global Values
+var UPARROW = "\u25B2";
+var DOWNARROW = "\u25BC";
+var RIGHTARROW = "\u25B6";
+var LEFTARROW = "\u25C0";
+var last_h_Toggle = 0;
+var last_v_Toggle = 0;
+
+function userLoaded(){
+ var myIframe = document.getElementById("medicineIframe");
+ var myBody = myIframe.contentWindow.document.body;
+ if(parseInt(window.screen.width) > 360){ 
+	myIframe.style.width = "570px"; 
+ } else {
+	myIframe.style.width = "330px";
+ }
+ myIframe.style.height = parseInt(myBody.scrollHeight) + "px";
+ fixPhones();
+ ageDOB();
+}
 
 function ageDOB(){
  var elements = document.querySelectorAll(".dob");
@@ -34,15 +51,85 @@ function fixPhones(){
  }
 }
 
-function userLoaded(){
- var myIframe = document.getElementById("medicineIframe");
- var myBody = myIframe.contentWindow.document.body;
- if(parseInt(window.screen.width) > 360){ 
-	myIframe.style.width = "570px"; 
- } else {
-	myIframe.style.width = "330px";
+/* hToggle(testname,counter)
+   Toggle columns that are blank for a specific test */
+function hToggle(myElement,which){
+ var whichOne = parseInt(which) - 1;
+ var myStyle = document.styleSheets[1];	//Second style sheet
+ var myRow= document.getElementById('labTable').
+	children[1].children[whichOne].children;//Get testname
+
+ /* If setting a different one, reset old one first */
+ if(last_h_Toggle && last_h_Toggle != myElement){	//If last different than this
+  for(var i=1; i<myRow.length; i++){		//Loop through labtable style
+   myStyle.cssRules[i].style.display="table-cell";//turn back on
+  }
+  last_h_Toggle.children[0].innerHTML = LEFTARROW;	//Reset icon
+  last_h_Toggle.children[0].style.color = 'black';	//and color
  }
- myIframe.style.height = parseInt(myBody.scrollHeight) + "px";
- fixPhones();
- ageDOB();
+
+ /* Now toggle this one. On or off */
+ var n = myElement.children[0].innerHTML;	//Find this row of values
+ if(n == RIGHTARROW){				//If off
+  for(var i=1; i<myRow.length; i++){
+   myStyle.cssRules[i].style.display="table-cell";//Turn on
+  }
+  myElement.children[0].innerHTML = LEFTARROW;//Set icon
+  myElement.children[0].style.color = 'black';	//and color
+ } else {
+  for(var i=1; i<myRow.length; i++){		//Check each row value
+   if(myRow[i].innerHTML == ""){		//If blank
+    myStyle.cssRules[i].style.display="none";	//Change cooresponding class
+   } else {					//to None
+    myStyle.cssRules[i].style.display="table-cell";//or turn on
+   }
+  }
+  myElement.children[0].innerHTML = RIGHTARROW;//Set icon
+  myElement.children[0].style.color = 'blue';	//and color
+ }  
+ last_h_Toggle = myElement;			//Remember the one we just did
 }
+
+function vToggle(myElement,which){
+ var whichOne = parseInt(which);
+ var myRows = document.getElementById('labTable').
+	children[1].children;
+ if(last_v_Toggle && last_v_Toggle != myElement){
+  last_v_Toggle.children[0].innerHTML = RIGHTARROW;
+  last_v_Toggle.children[0].style.color = "black";
+  for(i=0; i<myRows.length; i++){
+   myRows[i].parentElement.children[i].style.display='table-row';
+  }
+ } 
+
+ if(myElement.children[0].innerHTML == RIGHTARROW){
+  newstyle = "none";
+  myElement.children[0].innerHTML = LEFTARROW;
+  myElement.children[0].style.color = "blue";
+ } else {
+  newstyle = "table-row";
+  myElement.children[0].innerHTML = RIGHTARROW;
+  myElement.children[0].style.color = "black";
+ }
+ for(i=0; i<myRows.length; i++){
+  if(myRows[i].innerHTML.indexOf('panelName') == -1){
+   if(myRows[i].children[whichOne].innerHTML){
+    myRows[i].parentElement.children[i].style.display='table-row';
+   } else {
+    myRows[i].parentElement.children[i].style.display=newstyle;
+   }
+  }
+ }
+ last_v_Toggle = myElement;
+} 
+function panelToggle(myElement,which){
+ if(myElement.children[0].innerHTML == UPARROW){
+  d="none";
+  myElement.children[0].innerHTML = DOWNARROW;
+ } else {
+  d="table-row";
+  myElement.children[0].innerHTML = UPARROW;
+ }
+ var myRows = document.querySelectorAll("#pg"+which);
+ for(i=0; i<myRows.length; i++){ myRows[i].style.display=d;}
+} 
